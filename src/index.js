105d7510191a5aaa35b27529b85e6aa7bf4ef319
ewyrick-mark-1 <elliot@wyrick.ws>
2025-09-13 19:17:26 -0400

    built search and ran basic testing
    currently needs better error detection (maybe try/catch)

--- src/index.js
+++ src/index.js
@@ -3,15 +3,6 @@ const search = require('./search.js');
 const list = require('./list.js');
 const compare = require('./compare.js');
 
-//for debugging command arguments
-const args0 = process.argv[0];
-const args1 = process.argv[1];
-const args2 = process.argv[2];
-const args3 = process.argv[3];
-console.log("\nargv[0]: ", args0,"\nargv[1]: ", args1,"\nargv[2]: ", args2,"\nargv[3]: ", args3,);
-console.log("\nall :", process.argv);
-console.log("\nsplit :", process.argv.slice(2));
-
 const args = process.argv.slice(2);
 const size = args.length;
 for(let i = 0; i < size; i++){
--- src/search.js
+++ src/search.js
@@ -1,5 +1,52 @@
+//function to loop through types & return them as a single array
+function processTypes(types){
+    let typelist = [];
+    for(let i = 0; i < types.length; i++){
+        typelist.push(types[i].type.name);
+        console.log(types[i].type.name);
+    }
+    return typelist;
+}
+
+//function to fetchPokemon, outputs basic profile
+async function fetchPokemon(name){
+    console.log("searching for :", name);
+    let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`);
+    if(!pokemon.ok){//handles bad api request
+        console.log("something went wrong.\n");
+        console.log("API value: ", value);
+    }else{
+        pokemon = await pokemon.json();
+        
+        //builds array of needed values
+        let stats = {
+            "id": pokemon.id,
+            "name": pokemon.name,
+            "types": processTypes(pokemon.types).join(", "),
+            "height (m)": (pokemon.height * 0.1).toFixed(3), //adjusts to standard measurments & limits precision
+            "weight (kg)": (pokemon.weight * 0.1).toFixed(3),
+            "base hp": pokemon.stats[0].base_stat, // 0 is base hp
+            "base atk": pokemon.stats[1].base_stat,
+            "base def": pokemon.stats[2].base_stat,
+            "base SpA": pokemon.stats[3].base_stat,
+            "base SpD": pokemon.stats[4].base_stat,
+            "base Spe": pokemon.stats[5].base_stat,
+            
+        }
+        console.table(stats);
+        
+    }
+    
+    
+    
+}
+
 function search(args){
-    console.log("searching for :", args);
+    for(let i = 0; i < args.length; i++){
+        fetchPokemon(args[i]);
+    }
+    
+    
 }
 
 //exports it so the module may be recieved by main
